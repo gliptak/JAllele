1ae553c9fe31cb432781dc700c142f0350fbab42
Gabor Liptak <gliptak@users.sourceforge.net>
2010-04-13 20:30:53 -0400

    initial project layout

---
+++ src/main/java/com/github/gliptak/jallele/Agent.java
@@ -0,0 +1,30 @@
+package com.github.gliptak.jallele;
+
+import java.lang.instrument.Instrumentation;
+
+public class Agent
+{
+  /**
+   * A reference to the {@link java.lang.instrument.Instrumentation} instance
+   * passed to this agent's {@link #premain} method.  This way we can keep using
+   * the Instrumentation functionality!
+   **/
+  static private Instrumentation inst = null;
+
+  /**
+   * This method is called before the application’s main-method is called, when
+   * this agent is specified to the Java VM.
+   **/
+  public static void premain(String agentArgs, Instrumentation _inst)
+  {
+    System.out.println("Agent.premain() was called.");
+
+    // Initialize the static variables we use to track information.
+    inst = _inst;
+
+    // Set up the class-file transformer.
+    //ClassFileTransformer trans = new PerfMonXformer();
+    //System.out.println("Adding a PerfMonXformer instance to the JVM.");
+    //inst.addTransformer(trans);
+  }
+}
