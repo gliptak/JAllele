9de36273ae44d58bea397b7a492477d27a0a3599
Gabor Liptak <gliptak@users.sourceforge.net>
2010-07-26 20:15:07 -0400

    Correct typo in AgentTest.java

--- src/test/java/com/github/gliptak/jallele/AgentTest.java
+++ src/test/java/com/github/gliptak/jallele/AgentTest.java
@@ -52,13 +52,13 @@ public class AgentTest {
 	 * @throws Exception 
 	 */
 	@Test
-	public void testInsnttach() throws Exception {
+	public void testInstAttach() throws Exception {
 		Agent.attach();
 		Agent.attach();
 	}
 	
 	@Test
-	public void testInsnddRemoveTransformer() throws Exception {
+	public void testInstAddRemoveTransformer() throws Exception {
 		Agent.attach();
 		ClassRandomizer cr = new ClassRandomizer(new ArrayList<String>());
 		Agent.addTransformer(cr, true);
