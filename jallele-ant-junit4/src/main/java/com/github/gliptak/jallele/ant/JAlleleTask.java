2645c1ae506c34d66ecd478f50ec4d6d715eb17b
Gabor Liptak <gliptak@users.sourceforge.net>
2010-08-15 20:45:35 -0400

    Initial checkin for jallele-ant-junit4

---
+++ jallele-ant-junit4/src/main/java/com/github/gliptak/jallele/ant/JAlleleTask.java
@@ -0,0 +1,19 @@
+package com.github.gliptak.jallele.ant;
+
+import org.apache.tools.ant.taskdefs.optional.junit.JUnitTask;
+
+public class JAlleleTask extends JUnitTask {
+
+	public JAlleleTask() throws Exception {
+		super();
+	}
+
+	/* (non-Javadoc)
+	 * @see org.apache.tools.ant.taskdefs.optional.junit.JUnitTask#init()
+	 */
+	@Override
+	public void init() {
+		super.init();
+	}
+
+}
---
+++ jallele-ant-junit4/src/test/java/com/github/gliptak/jallele/ant/SimpleClass.java
@@ -0,0 +1,12 @@
+package com.github.gliptak.jallele.ant;
+
+public class SimpleClass {
+
+	public int twoTimes(int i){
+		return 2*i;
+	}
+	
+	public int plusTwo(int i){
+		return 2+i;
+	}
+}
