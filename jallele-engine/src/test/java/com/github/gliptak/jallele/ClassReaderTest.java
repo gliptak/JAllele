4d29aa3ea56f166ad92f7c4a6276889cfd144d66
Gábor Lipták <gliptak@gmail.com>
2021-01-30 17:08:37 -0500

    Add ClassReaderTest back to basics

---
+++ jallele-engine/src/test/java/com/github/gliptak/jallele/ClassReaderTest.java
@@ -0,0 +1,38 @@
+package com.github.gliptak.jallele;
+
+import com.github.gliptak.jallele.testStack.IStore;
+import org.junit.Test;
+import org.objectweb.asm.*;
+
+import java.io.IOException;
+
+public class ClassReaderTest {
+    @Test
+    public void testClassReader() throws IOException {
+        ClassReader cr = new ClassReader(IStore.class.getName());
+        ClassWriter cw = new ClassWriter(0);
+        ClassVisitor cv = new ClassVisitor(Opcodes.ASM5, cw) {
+            @Override
+            public MethodVisitor visitMethod(final int access,
+                                             final String name, final String desc,
+                                             final String signature, final String[] exceptions) {
+                MethodVisitor mv=super.visitMethod(access, name, desc, signature,
+                        exceptions);
+                return null;
+            }
+        };
+        cr.accept(cv, 0);
+        System.out.println(bytesToHex(cw.toByteArray()));
+    }
+
+    private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
+    public static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for (int j = 0; j < bytes.length; j++) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = HEX_ARRAY[v >>> 4];
+            hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
+}
